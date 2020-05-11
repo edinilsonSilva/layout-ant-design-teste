129a6d7723512bd787ab3bb68532d24cfd5023da
edinilsonSilva <bateraed@gmail.com>
2020-05-11 01:17:13 -0300

    Initial Commit

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './App.css';
+import LayoutPage from '../containers/layout/LayoutPage'
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutPage />
+    </div>
+  );
+}
+
+export default App;
