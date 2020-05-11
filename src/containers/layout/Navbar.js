759358c96d46cebbde85a2b01f82da2d378d3353
edinilsonSilva <bateraed@gmail.com>
2020-05-11 02:13:32 -0300

    Correção CSS Navbar

--- src/containers/layout/Navbar.js
+++ src/containers/layout/Navbar.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
-import { Layout, Avatar } from 'antd';
+import { Layout, Avatar, Button } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 
 
 function Navbar() {
 
-    const { Header} = Layout;
+    const { Header } = Layout;
 
     return (
-        <Header className="header">
+        <Header className="header" >
             <div className="logo" />
             {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
             <Menu.Item key="1">Cadastro</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
         </Menu> */}
+            <div className="usuarioLogado">
+                <Button size="small" style={{ margin: '0 16px', verticalAlign: 'middle' }}>
+                    Edinilson Gonçalves da Silva
+                </Button>
+            </div>
             <div className="avatar">
                 <Avatar icon={<UserOutlined />} size="large" />
-            </div>
+                </div>
         </Header>
     )
 }
